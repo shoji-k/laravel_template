9677665d2bfcecc7791ba5b788c1da6305ab6356
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-09-08 19:47:27 +0900

    add declare(strict_types=1);

--- tests/Feature/TodosTest.php
+++ tests/Feature/TodosTest.php
@@ -1,9 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodosTest extends TestCase
