fa597338e1e8087b86fe06114be8402853dcd2a6
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-09-10 18:05:14 +0900

    add testAccessTodosEdit

--- tests/Feature/TodoTest.php
+++ tests/Feature/TodoTest.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use App\Models\Todo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class TodoTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @return void
      */
@@ -21,10 +25,21 @@ class TodoTest extends TestCase
     /**
      * @return void
      */
-    public function testAccessTodosNew()
+    public function testAccessTodosCreate()
     {
         $response = $this->get('/todos/create');
         $response->assertSeeText('Create Todo');
+        $response->assertStatus(200);
+    }
+
+        /**
+     * @return void
+     */
+    public function testAccessTodosEdit()
+    {
+        $todo = Todo::factory()->create();
+        $response = $this->get("/todos/{$todo->id}/edit");
+        $response->assertSeeText('Edit Todo');
         $response->assertStatus(200);
     }
 }
