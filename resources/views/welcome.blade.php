cf1f29bc9491bebceeaa9a4370c9ad191ed3b7c8
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-10-23 21:59:46 +0900

    fix dashboard link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -404,8 +404,10 @@
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
             <span>Hi, {{ \Auth::user()->name }}</span>
-            <a href="{{ url('/') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-            <a href="{{ route('logout') }}" class="text-sm text-gray-700 underline">Log out</a>
+            <a href="{{ route('dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+            <form method="POST" action="{{ route('logout') }}">
+                <a href="{{ route('logout') }}" class="text-sm text-gray-700 underline">Log out</a>
+            </form>
             @else
             <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
