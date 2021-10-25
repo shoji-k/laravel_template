0f2da2b898b1379c67175f9271944f10666df914
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-10-25 17:15:39 +0900

    prepare admin layout

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::prefix('admin')->group(function () {
         Route::get('/dashboard', function () {
-            return 'Hello World';
-        });
+            return view('admin.dashboard');
+        })->name('admin.dashboard');
     });
 });
