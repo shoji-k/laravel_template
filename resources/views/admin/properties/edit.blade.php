80f033f54cd10bb3f7313f1127c0368cfd76470d
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-11-02 15:02:23 +0900

    improve layout

--- resources/views/admin/properties/edit.blade.php
+++ resources/views/admin/properties/edit.blade.php
@@ -1,26 +1,32 @@
 <x-layout>
     <x-slot name="title">Edit Property</x-slot>
-    <h1>Edit Property</h1>
-    <div>
-        <a href="{{ route('admin.properties.index') }}">Back to list</a>
+    <h1 class="mb-2">Edit Property</h1>
+    <div class="mb-2">
+        <a href="{{ route('admin.properties.index') }}" class="hover:underline">Back to list</a>
     </div>
-    <div>
+    <div class="mb-4">
         <form name="editform" action="{{ route('admin.properties.update', ['property' => $data->id]) }}" method="post" id="editform">
             @method('PUT')
             @csrf
-            <dl>
-                <dt>Name</dt>
-                <dd>
-                    <x-input id="name" type="text" name="name" value="{{ old('name', $data->name) }}" class="block mt-1 w-full" required />
+            <dl class="mb-2">
+                <dt class="mb-1">Name</dt>
+                <dd class="mb-1">
+                    <x-input id="name" type="text" name="name" value="{{ old('name', $data->name) }}" class="block" required />
                     <span style="color: red;">{{ $errors->first('name') }}</span>
                 </dd>
             </dl>
-            <button type="submit" name="action" value="send">Update</button>
+            <x-button>
+                {{ __('Update') }}
+            </x-button>
         </form>
+    </div>
+    <div>
         <form method="POST" action="{{ route('admin.properties.destroy', ['property' => $data->id]) }}">
             @method('DELETE')
             @csrf
-            <button type="submit">Delete</button>
+            <x-button>
+                {{ __('Delete') }}
+            </x-button>
         </form>
     </div>
 </x-layout>
