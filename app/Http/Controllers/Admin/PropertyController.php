915724555037ecd0e6ed9443a837df36fceb984a
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2021-11-02 16:15:50 +0900

    can delete property

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -86,6 +86,7 @@ class PropertyController extends \App\Http\Controllers\Controller
      */
     public function destroy(Property $property)
     {
-        //
+        $property->delete();
+        return redirect()->route('admin.properties.index');
     }
 }
